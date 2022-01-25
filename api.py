2a579cff7f9daafb93cb9e2dc4715476dd3cc063
Antoine Pinault <pinaultae@gmail.com>
2022-01-25 11:49:30 +0100

    modification fichier api.py

--- api.py
+++ api.py
@@ -1,37 +1,46 @@
+import pickle
+import pandas as pd
 from flask import Flask, request, jsonify
-import joblib
-import os
 
-# Connexion au modèle: sur PythonAnywhere
-#my_dir = os.path.dirname(__file__)
-#file_path = os.path.join(my_dir, 'best_model.pkl')
-#with open(file_path, 'rb') as f:
-#    saved_mdl = joblib.load(f)
+# Load pretrained model
+transformer = pickle.load(open("transformer.pkl", "rb"))
+classifier = pickle.load(open("classifier.pkl", "rb"))
 
-# Définition de l'API
+# Init Flask app
 app = Flask(__name__)
 
+@app.route('/git_update', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('./OpenClassrooms-P7')
+            origin = repo.remotes.origin
+            repo.create_head('main',origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+            origin.pull()
+            return '', 200
+        else:
+            return '', 400
 
 @app.route("/")
 def hello():
-    return "Temporary page"
+    return "Machine learning API"
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=['POST','GET'])
 def predict():
-    json_ = request.get_json()
-    data_json = json_['data']
-    data_val = [list(data_json.values())]
-    predict_val = saved_mdl.predict(data_val)
-    prediction = predict_val.tolist()
-    proba_val = saved_mdl.predict_proba(data_val)
-    probabilite = proba_val.tolist()
+    if request.method == 'GET':
+        return "Prediction page"
 
-    return jsonify(Prediction=prediction, Probabilite=probabilite)
-
+    if request.method == 'POST':
+        client_input = request.get_json()
+        # Convert dictionary to pandas dataframe
+        client_input = pd.DataFrame(client_input)
+        # Transforming features
+        client_input = transformer.transform(client_input)
+        # Making predictions
+        pred = classifier.predict(client_input)[0]
+        proba = classifier.predict_proba(client_input)[0][pred]
+        return jsonify(prediction=int(pred), probability=round(100 * proba, 1))
 
 if __name__ == '__main__':
-    # Pour une utilisation en local + mettre en comm la connexion PythonAnywhere
-    saved_mdl = joblib.load('best_model.pkl') # Chargement du modèle
     # Si en cours de DEV, mettre debug=True. Si en PROD, ne pas le mettre
     app.run(debug=True)
     #app.run(debug=False)
