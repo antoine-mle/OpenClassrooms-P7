10dd98950ea049b37f70ddfbecdb876825550a7b
Antoine Pinault <pinaultae@gmail.com>
2022-02-02 19:24:24 +0100

    ajout comparatif client

--- app.py
+++ app.py
@@ -70,9 +70,16 @@ def main():
     st.sidebar.header("Parameters:")
     page = st.sidebar.selectbox(
         "Choose an option:",
-        ["Data Analysis", "Prediction", "Feature Importance"],
+        ["Data Analysis", "Prediction", "Client Comparison", "Feature Importance"],
         index=0,
     )
+
+    df_analysis = df.copy()
+    for col in df_analysis.filter(like="DAYS").columns:
+        df_analysis[col] = df_analysis[col].apply(lambda x: abs(x / 365))
+    df_analysis.columns = df_analysis.columns.str.replace("DAYS", "YEARS")
+    df_analysis["TARGET"] = df_analysis["TARGET"].astype(str)
+    choice_list = list(df_analysis.iloc[:, 2:].columns)
 
     if page == "Data Analysis":
         st.title("Data Exploration")
@@ -82,13 +89,6 @@ def main():
             index=0,
         )
 
-        df_analysis = df.copy()
-        for col in df_analysis.filter(like="DAYS").columns:
-            df_analysis[col] = df_analysis[col].apply(lambda x: abs(x / 365))
-        df_analysis.columns = df_analysis.columns.str.replace("DAYS", "YEARS")
-        df_analysis["TARGET"] = df_analysis["TARGET"].astype(str)
-        choice_list = list(df_analysis.iloc[:, 2:].columns)
-
         if data_analysis == "Univariate":
             st.header("Univariate Analysis")
             options = st.multiselect(
@@ -96,7 +96,7 @@ def main():
                 choice_list,
                 ["AMT_INCOME_TOTAL", "AMT_CREDIT", "NAME_FAMILY_STATUS", "NAME_EDUCATION_TYPE", "YEARS_BIRTH"])
 
-            if df_analysis[options].select_dtypes(include=['int64', 'float64']).shape[1] > 0:
+            if df_analysis[options].select_dtypes(include=["int64", "float64"]).shape[1] > 0:
                 graphic_style = st.sidebar.radio(
                     "Select a graphic style for numerical features",
                     ("Histogram", "Box Plot"),
@@ -222,7 +222,7 @@ def main():
                 "Select a graphic style for numerical features",
                 ("Correlation Matrix", "Scatter Plot")
             )
-            num_choice_list = list(df_analysis.iloc[:, 2:].select_dtypes(include=['int64', 'float64']).columns)
+            num_choice_list = list(df_analysis.iloc[:, 2:].select_dtypes(include=["int64", "float64"]).columns)
 
             if graphic_style == "Correlation Matrix":
                 container = st.container()
@@ -304,11 +304,12 @@ def main():
                 st.plotly_chart(fig, use_container_width=True)
 
 
-
-
     elif page == "Prediction":
         sorted_ids = sorted(ids)
-        client_id = st.sidebar.selectbox("Select a client ID:", sorted_ids)
+        client_id = st.sidebar.selectbox(
+            "Select a client ID:",
+            sorted_ids,
+        )
         id_idx = ids.index(client_id)
         client_input = X_test.iloc[[id_idx], :]
 
@@ -355,6 +356,73 @@ def main():
             ).astype(str).transpose()
             st.dataframe(df_client_input)
 
+    elif page == "Client Comparison":
+        sorted_ids = sorted(ids)
+        client_id = st.sidebar.selectbox(
+            "Select a client ID:",
+            sorted_ids,
+        )
+
+        st.title("Behavior comparison")
+
+        col_choice_list = list(df_analysis.iloc[:, 2:].select_dtypes(include=["object"]).columns)
+        num_choice_list = list(df_analysis.iloc[:, 2:].select_dtypes(include=["int64", "float64"]).columns)
+
+        st.write("Choose categorical groups and numerical variables to compare client #{} with similar clients".format(client_id))
+        col1, col2 = st.columns(2)
+
+        feat_1 = col1.multiselect(
+            "Categorical variables to identify groups",
+            col_choice_list,
+            ["CODE_GENDER", "NAME_INCOME_TYPE", "NAME_EDUCATION_TYPE", "NAME_FAMILY_STATUS"]
+        )
+
+        feat_2 = col2.multiselect(
+            "Numerical variables",
+            num_choice_list,
+            ["AMT_CREDIT", "AMT_ANNUITY", "AMT_INCOME_TOTAL", "YEARS_BIRTH", "YEARS_EMPLOYED"]
+        )
+
+        df_group = df_analysis.copy()
+        mask = (df_group[feat_1].values == df_group[df_group["SK_ID_CURR"] == client_id][feat_1].values)
+        indices = np.where(((np.invert(mask) * 1).sum(axis=1)) == 0)[0]
+        df_group = df_group.iloc[indices, :]
+        df_comparison = pd.DataFrame(columns=feat_2)
+        df_comparison.loc["Selected client"] = df_group[df_group["SK_ID_CURR"] == client_id][feat_2].mean()
+        df_comparison.loc["Clients having refunded (Mean)"] = df_group[(df_group["TARGET"] == "0") & (df_group["SK_ID_CURR"] != client_id)][feat_2].mean()
+        df_comparison.loc["Defaulted clients (Mean)"] = df_group[(df_group["TARGET"] == "1") & (df_group["SK_ID_CURR"] != client_id)][feat_2].mean()
+        df_comparison = df_comparison.round(2).rename_axis("Group").reset_index()
+
+        fig = px.parallel_coordinates(
+            data_frame=df_comparison,
+            dimensions=feat_2,
+            color=(1 + df_comparison.index),
+            range_color=[0.5, 3.5],
+            color_continuous_scale=[(0.00, px.colors.diverging.Tealrose[0]),
+                                    (0.33, px.colors.diverging.Tealrose[0]),
+                                    (0.33, px.colors.diverging.Tealrose[3]),
+                                    (0.66, px.colors.diverging.Tealrose[3]),
+                                    (0.66, px.colors.diverging.Tealrose[-1]),
+                                    (1.00, px.colors.diverging.Tealrose[-1])]
+        )
+
+        fig.update_layout(coloraxis_colorbar=dict(
+            title="Group",
+            tickvals=[1, 2, 3],
+            ticktext=df_comparison["Group"],
+            lenmode="pixels", len=100,
+        ))
+
+        st.plotly_chart(fig, use_container_width=True)
+
+        with st.expander("Show client group information:"):
+            df_selected_client = pd.DataFrame(
+                df_group[df_group["SK_ID_CURR"] == client_id][feat_1].to_numpy(),
+                index=["Information"],
+                columns=feat_1,
+            ).astype(str)
+            st.dataframe(df_selected_client)
+
     elif page == "Feature Importance":
         st.title("Feature importance for prediction")
 
@@ -363,7 +431,7 @@ def main():
             shap_values=explainer.shap_values,
             features=features,
             feature_names=feature_names,
-            plot_type='bar',
+            plot_type="bar",
         )
         st.pyplot(summary_plot)
 
